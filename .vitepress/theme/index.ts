961650611b85a69ed614b5f5f51b76d526aff72b
nikechan-x-worker <nikechan-x-worker@example.local>
2026-06-20 22:12:00 +0200

    docs: remove hardcoded document version banner

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,15 +2,13 @@ import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
 import HomeBadges from './HomeBadges.vue'
 import HomeNotice from './HomeNotice.vue'
-import DocVersion from './DocVersion.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
-      'home-features-before': () => [h(HomeBadges), h(HomeNotice)],
-      'layout-bottom': () => h(DocVersion)
+      'home-features-before': () => [h(HomeBadges), h(HomeNotice)]
     })
   }
 }
